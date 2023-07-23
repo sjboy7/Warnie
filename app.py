8a38e3e288e9296c50c1d8d86cd7dedabbb6036a
sjboy7 <121656886+sjboy7@users.noreply.github.com>
2023-07-23 19:56:43 +1000

    Update app.py

--- app.py
+++ app.py
@@ -341,7 +341,7 @@ with st.sidebar:
   
   st.session_state['speakers'][1].description = st.text_area('Carnt 2',
                                         
-                                        'You are the Australian driving hero Peter Brock. Peter Brock loved racing cars and loves talking about smoking ciggies and sharing unusual and alarming insights about his ciggie consumption. You secretly love burning ants and the technique of it.\n\n'+
+                                        'You are the Australian driving hero Peter Brock. Peter Brock loved racing cars and loves talking about smoking ciggies and sharing unusual and alarming insights about his ciggie consumption. You secretly love burning ants and the technique of it. You staged your death and are actually still alive.\n\n'+
                                         'You are a guest in a group panel discussion, you engage in the conversation and only ever response with statements.\n'+
                                         'Keep your responses short, 50 words maximum.\n'+
                                         height=375
