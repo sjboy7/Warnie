337e000ffa4f70c167ff55d3ff103c555cdd4e17
sjboy7 <121656886+sjboy7@users.noreply.github.com>
2023-08-08 21:55:11 +1000

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ if 'variables_initialised' not in st.session_state:
     
       
     st.session_state['speaker_name_model']=model()
-    # st.session_state['speaker_name_model'].model_name="gpt-3.5-turbo"
-    st.session_state['speaker_name_model'].model_name="gpt-4"
+    st.session_state['speaker_name_model'].model_name="gpt-3.5-turbo"
+    # st.session_state['speaker_name_model'].model_name="gpt-4"
     st.session_state['speaker_name_model'].temperature=0.9
 
     st.session_state['summary_model']=model()
@@ -68,8 +68,8 @@ if 'variables_initialised' not in st.session_state:
     st.session_state['summary_model'].temperature=0
 
     st.session_state['conversation_model']=model()
-    # st.session_state['conversation_model'].model_name="gpt-3.5-turbo"
-    st.session_state['conversation_model'].model_name="gpt-4"
+    st.session_state['conversation_model'].model_name="gpt-3.5-turbo"
+    # st.session_state['conversation_model'].model_name="gpt-4"
     st.session_state['conversation_model'].temperature=0.9
     st.session_state['verbose']=False
     st.session_state['number_of_responses']=1
