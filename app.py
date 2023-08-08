0064d83cf7683a411c81e9b3e1ca53e6eed8e3f1
sjboy7 <121656886+sjboy7@users.noreply.github.com>
2023-08-08 22:52:37 +1000

    Update app.py

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ with st.sidebar:
   )
   
   os.environ['OPENAI_API_KEY'] = st.text_input("OpenAI API Key")
-  st.markdown('Usage (USD): '+ "{:.4f}".format(st.session_state.total_cost_counter))
+  st.markdown('Usage (USD): '+ "{:.2f}".format(st.session_state.total_cost_counter))
   # st.markdown('Prompt tokens: '+ "{:.0f}".format(st.session_state.prompt_token_counter))
   # st.markdown('Completion tokens: '+ "{:.0f}".format(st.session_state.completion_token_counter))
 
